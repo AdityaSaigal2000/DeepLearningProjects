b17ddd33764aa2b3fed732a801e604dbc4a2f8c9
AdityaSaigal2000 <aditya.saigal@mail.utoronto.ca>
2020-12-18 21:27:11 -0500

    Updated driver code to run on a CPU in case a machine does not have a GPU. Fixed the layer names in the model architecture to match name_classifier.pt

--- Name Classifier/predictor.py
+++ Name Classifier/predictor.py
@@ -19,31 +19,31 @@ class name_predictor(nn.Module):
         super(name_predictor, self).__init__()
         # Using 3 LSTM layers. Feed the output at the last time step to 2 fully connected layers.
         self.lstm = nn.LSTM(input_size = input_dim, hidden_size = hidden_dim, num_layers = 3, batch_first = True)
-        self.dense1 = nn.Linear(in_features = hidden_dim, out_features = 20, bias = True)
-        self.dense2 = nn.Linear(in_features = 20, out_features = num_targets, bias = True)
+        self.hidden_2_lang = nn.Linear(in_features = hidden_dim, out_features = 20, bias = True)
+        self.dense = nn.Linear(in_features = 20, out_features = num_targets, bias = True)
 
         #Using Xavier initialization on the FC layers and initializing the LSTM parmaters from a normal dist. Tried other initializations but this worked the best.
         for param in self.lstm.parameters():
             torch.nn.init.normal(param.data)
 
-        torch.nn.init.xavier_uniform(self.dense1.weight)
-        torch.nn.init.xavier_uniform(self.dense2.weight)
+        torch.nn.init.xavier_uniform(self.dense.weight)
+        torch.nn.init.xavier_uniform(self.hidden_2_lang.weight)
 
     def forward(self, names, hidden):
-        return self.dense2(F.relu(self.dense1(F.relu(self.lstm(names, hidden)[1][1][0]))))
+        return self.dense(F.relu(self.hidden_2_lang(F.relu(self.lstm(names, hidden)[1][1][0]))))
 
 #Given a name, use the model to determine the odds that it has a certain origin
 def predict(name, model):
-    hidden = (torch.zeros(3, 1, 80).to(torch.device(device)), torch.zeros(3, 1, 80).to(torch.device(device)))
+    hidden = (torch.zeros(3, 1, 80).to(torch.device("cpu")), torch.zeros(3, 1, 80).to(torch.device("cpu")))
     lang = ["Russian", "Indian", "English", "Oriental"]
     name = encode_name((19 - len(name)) * "0" + name).unsqueeze(0)
-    forward = model(name.to(torch.device(device)), hidden) 
+    forward = model(name.to(torch.device("cpu")), hidden) 
     answer = list(F.softmax(forward) * 100)[0]
 
     for i in range(len(answer)):
         print(lang[i] + " : " + str(round(float(answer[i]), 2)) + "%")
 
 model = name_predictor(len(all_letters), 80, 4)
-checkpoint = torch.load("./name_classifier.pt")
+checkpoint = torch.load("./name_classifier.pt", map_location = torch.device("cpu"))
 model.load_state_dict(checkpoint["model_state_dict"])
 predict("Xiao", model)
